1bcde0a8bab006295b21c23911e9694860009061
PigKidney <tony577@naver.com>
2023-12-15 17:09:29 +0900

    23/12/15 pm

--- js/resource/js17.js
+++ js/resource/js17.js
@@ -76,7 +76,7 @@ const getGrade = (score) => {
     }
 }
 
-const students =  scores.map((score) => ({score: score, grade: getGrade(score)}));
+const students = scores.map((score) => ({ score: score, grade: getGrade(score) }));
 
 console.log(resultArr);
 console.log(grades);
@@ -95,9 +95,9 @@ console.log(students);
 */
 
 // 60점 미만인 점수만 남겨놓고 싶은 경우
-const filtered = scores.filter((score) => {return score < 60;});
+const filtered = scores.filter((score) => { return score < 60; });
 // 등급이 F인 객체만 남겨놓고 싶은 경우
-const fs = students.filter((student) => {return student.grade === 'F';});
+const fs = students.filter((student) => { return student.grade === 'F'; });
 console.log(filtered);
 console.log(fs);
 
@@ -130,7 +130,7 @@ const min = scores.reduce((minScore, currScore) => {
 });
 
 
-const max = scores.reduce((maxScore, currScore)=> maxScore > currScore ? maxScore : currScore);
+const max = scores.reduce((maxScore, currScore) => maxScore > currScore ? maxScore : currScore);
 
 console.log('총합 : ', sum);
 console.log('가장 낮은 점수 : ', min);
@@ -144,7 +144,7 @@ console.log('가장 높은 점수 : ', max);
 
 */
 
-const fruits = ['apple','banana','orange','melon', 'pineapple'];
+const fruits = ['apple', 'banana', 'orange', 'melon', 'pineapple'];
 
 // 과일 중에 참외가 있는지 검사해보기
 const result1 = fruits.some((fruit) => {
@@ -172,12 +172,85 @@ console.log(result3);
 
 */
 
-const firstName = ['김','이','박','윤', '황','권','양','최','강','조','정','신'];
+const firstName = ['김', '이', '박', '윤', '황', '권', '양', '최', '강', '조', '정', '신'];
+const lastName = ['민준', '서준', '도윤', '예준', '시우', '하준', '지호', '주원', '지후', '준우', '서윤', '서연', '지우', '하윤', '서현', '하은', '민서', '지유', '윤서', '채원'];
 
-const lastName = ['민준','서준','도윤','예준', '시우','하준','지호','주원','지후','준우','서윤','서연','지우','하윤','서현','하은','민서','지유','윤서','채원'];
+const getRanfirstName = () => firstName[parseInt(Math.random() * firstName.length)];
+const getRanlastName = () =>  lastName[parseInt(Math.random() * lastName.length)];
+const getRanName = () => getRanfirstName() + getRanlastName();
+const getRanScore = () =>  parseInt(Math.random() * 101);
+
+
+let stuNum = 0;
+
+function Student() {
+    this.name = getRanName();
+    this.studentNum = `STU${stuNum++}`;
+    this.kor = getRanScore();
+    this.eng = getRanScore();
+    this.math = getRanScore();
+
+}
+
+const students2 = [];
+
+for (let i = 0; i < 100; ++i) {
+   students2.push(new Student());
+}
+
+console.log(students2);
+
+// 모든 학생들에게 평균 필드 추가하기
+students2.forEach(stu => stu.avg = (stu.kor + stu.eng + stu.math) / 3 );
+
+console.log(students2);
+
+// 평균 점수 60점 미만 학생 배열 만들기
+const fstus = students2.filter(stu => stu.avg < 60);
+console.log(fstus);
+
+// 각 과목 평균점수 계산하기
+// 1. 총점을 구하는 함수
+// 2. 총점을 전체 길이로 나눠서 평균을 구하는 함수
+
+// 총합 결과에 .kor을 할 수 없으므로 문제가 생김...
+// students2.reduce((stu1, stu2) => stu1.kor + stu2.kor);
+
+// reduce에 두 번째 인자값으로 초기값을 전달해주면
+// 첫 번째 반복을 초기값으로 진행할 수 있음
+const getTotal = sub =>  students2.reduce((tot, stu) => tot + stu[sub], 0);
+
+console.log('국어 총합:', getTotal('kor'));
+console.log('영어 총합:', getTotal('eng'));
+console.log('수학 총합:', getTotal('math'));
+
+const getSubjectAvg = sub => getTotal(sub) / students2.length;
+
+console.log('국어 평균:', getSubjectAvg('kor'));
+console.log('영어 평균:', getSubjectAvg('eng'));``
+console.log('수학 평균:', getSubjectAvg('math'));
+
+const allStudentsTable = document.getElementById('all-students');
+const badStudentsTable = document.getElementById('bad-students');
+
+
+const addRow = (table,stu) => {
+     table.innerHTML +=
+      `<div>${stu.studentNum}</div>`
+      +`<div>${stu.name}</div>`
+      +`<div>${stu.kor}</div>`
+      +`<div>${stu.eng}</div>`
+      +`<div>${stu.math}</div>`
+      +`<div>${stu.avg}</div>`
+}
+
+students2.forEach(stu => addRow(allStudentsTable, stu));
+fstus.forEach(stu => addRow(badStudentsTable, stu))
+
+/*
 
 let count = 0;
-function RandomStudents(){
+function RandomStudents() {
     this.name = getRanName();
     this.studentNum = `EZ00${count++}`;
     this.korS = getRanScore();
@@ -185,46 +258,111 @@ function RandomStudents(){
     this.mathS = getRanScore();
 }
 
+
+
 function getRanScore() {
-    return parseInt((Math.random()*100)+1);
+    return parseInt((Math.random() * 100) + 1);
 };
 
 function getRanName() {
-    return firstName[parseInt(Math.random()*firstName.length)]+lastName[parseInt(Math.random()*lastName.length)];
+    return firstName[parseInt(Math.random() * firstName.length)] + lastName[parseInt(Math.random() * lastName.length)];
 };
-// const maxSize = 100;
-// const school = new Array(maxSize);
+
+
 const school = [];
 for (let i = 0; i < 100; ++i) {
-    school[i] = i;
+    school[i] = new RandomStudents();
+    school[i].avg = parseInt((school[i].korS + school[i].engS + school[i].mathS) / 3);
+
 }
 
-// school[0].__proto__.avg = function(){
-//     this.avg = school[0].korS + school[0].engS + school[0].mathS;
-    
-// }
-// school[0].avg();
+console.dir(school);
 
-const getRanStudent = function (item, index) {
-    item = new RandomStudents();
-    item.avg = parseInt((item.korS + item.engS + item.mathS)/3);
-    console.log(`[${index}].Student [${item.name}]: ${item.studentNum} ${item.korS} ${item.engS} ${item.mathS} 평균 :${item.avg}`);
+school.avg = function() {
+    console.log(parseInt((school[i].korS + school[i].engS + school[i].mathS) / 3));
 }
 
+let koravg = 0;
+let engavg = 0;
+let maxavg = 0;
+let mathavg = 0;
+for (let i = 0; i < 100; ++i) {
+    maxavg += school[i].avg
+    engavg += school[i].engS
+    koravg += school[i].korS
+    mathavg += school[i].mathS
+}
 
-school.map(getRanStudent);
-// console.log(school.avg);
-
-
-
-const failScore = school.filter((student) => {return student.avgs < 60;});
-
-const totalEngSSum = school.reduce((precScore, currScore, index) => {
-    console.log(`## ${index}번째 반복 ##`);
-    console.log('prev:', school[index].engS);
-    console.log('curr:', currScore.engS);
-
-    return precScore.engS + currScore.engS;
+const maxscore = school.reduce((precstudent, currstudent) => {
+    console.log(precstudent.engS);
+    console.log(currstudent.engS);
+    return precstudent.engS + currstudent.engS;
 });
-console.log(totalEngSSum);
-console.log(failScore);
+
+console.log('총합 : ', maxscore);
+
+koravg = parseInt(koravg / 100);
+engavg = parseInt(engavg / 100);
+maxavg = parseInt(maxavg / 100);
+mathavg = parseInt(mathavg / 100);
+console.log('전체 학생 평균 : ', maxavg);
+console.log('영어 점수 평균 : ', engavg);
+console.log('국어 학생 평균 : ', koravg);
+console.log('수학 점수 평균 : ', mathavg);
+
+
+let failScore = school.filter((student) => student.avg < 60);
+console.dir(failScore);
+
+
+
+
+
+const schooltable = document.getElementById('school');
+schooltable.style.textAlign = 'center';
+schooltable.style.width = '500px'
+schooltable.style.margin = '1px'
+schooltable.style.borderCollapse = 'collapse'
+
+
+
+
+// schooltable.innerHTML += `<tr><th>${school[0].num}</th></tr>`;
+schooltable.innerHTML +=
+    `<tr style="background-color: black; color: white;" ><th>학번</th><th>이름</th><th>국어</th><th>영어</th><th>수학</th><th>평균</th></tr>`;
+
+function addstudentInfo() {
+
+    for (let i = 0; i < 100; i++) {
+        schooltable.innerHTML +=
+            `<tr><td>${school[i].studentNum}</td> <td>${school[i].name}</td><td>${school[i].korS}</td>
+        <td>${school[i].engS}</td><td>${school[i].mathS}</td><td>${school[i].avg}</td></tr>`;
+    }
+
+}
+addstudentInfo();
+
+schooltable.innerHTML +=
+    `<tr><th colspan="6" style="background-color: black; color: white;" >평균 점수 60점 미만 학생들</th></tr>`;
+
+function addfailStudent() {
+
+    for (let i = 0; i < failScore.length; i++) {
+        schooltable.innerHTML +=
+            `<tr><td>${failScore[i].studentNum}</td> <td>${failScore[i].name}</td><td>${failScore[i].korS}</td>
+        <td>${failScore[i].engS}</td><td>${failScore[i].mathS}</td><td>${failScore[i].avg}</td></tr>`;
+    }
+
+}
+
+addfailStudent();
+
+schooltable.innerHTML +=
+    `<tr><th colspan="6"  style="background-color: black; color: white;" >학생들 각점수 평균</th></tr>`;
+schooltable.innerHTML +=
+    `<tr><th colspan="2">국어평균</th><th colspan="2">영어평균</th><th colspan="2">수학평균</th></tr>`;
+
+schooltable.innerHTML +=
+    `<tr><td colspan="2">${koravg}</td><td colspan="2">${engavg}</td><td colspan="2">${mathavg}</td></tr>`;
+
+ */
