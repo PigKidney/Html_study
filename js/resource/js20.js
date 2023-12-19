e387297dd5c4906bd3789f2697b88e62553cabcf
PigKidney <tony577@naver.com>
2023-12-19 12:47:22 +0900

    23/12/19 am

--- js/resource/js20.js
+++ js/resource/js20.js
@@ -1,9 +1,90 @@
 const pushBtn = document.querySelector('#pushBtn');
 const popBtn = document.querySelector('#popBtn');
+const shiftBtn = document.querySelector('#shiftBtn');
+const unShiftBtn = document.querySelector('#unShiftBtn');
 
 const out = document.querySelector('#out');
 
+let starCount = 0;
+const createStarDiv = () => {
+    const starDiv = document.createElement('div');
+    const starText = document.createTextNode('grade');
+    starDiv.classList.add('material-symbols-outlined');
+    starDiv.classList.add('star');
+    starDiv.appendChild(starText);
 
+    const numDiv = document.createElement('div');
+    const numText = document.createTextNode(starCount++);
+
+    numDiv.classList.add('starNum');
+    numDiv.appendChild(numText);
+    starDiv.appendChild(numDiv);
+
+    return starDiv;
+}
+
+const setFirstChild = () => document.querySelector('#out > div:first-child');
+
+const pushChild = () => out.appendChild(createStarDiv());
+const popChild = () => out.removeChild(out.lastElementChild);
+const shiftChild = () => out.removeChild(out.firstElementChild);
+const unShiftChild = () => out.insertBefore(createStarDiv(), out.firstElementChild);
+
+
+pushBtn.addEventListener('click', (e) => pushChild());
+popBtn.addEventListener('click', (e) => popChild())
+shiftBtn.addEventListener('click', (e) => shiftChild());
+unShiftBtn.addEventListener('click', (e) => unShiftChild())
+
+document.body.addEventListener('keydown', (e) => {
+
+    switch (e.key) {
+        case "ArrowRight":
+            e.preventDefault();
+            unShiftChild();
+            break;
+        case "ArrowLeft":
+            e.preventDefault();
+            shiftChild();
+            break;
+        case "ArrowUp":
+            e.preventDefault();
+            pushChild();
+            break;
+        case "ArrowDown":
+            e.preventDefault();
+            popChild();
+            break;
+
+    }
+});
+
+// window.addEventListener('keydown', (e) => {
+
+//     if (e.key == 37 || e.key == "ArrowRight") {
+//         unShiftChild();
+//     }
+//     else if (e.key == 39 || e.key == "ArrowLeft") {
+//         shiftChild();
+//     }
+//     else if (e.key == 38 || e.key == "ArrowUp") {
+//         pushChild();
+//     }
+//     else if (e.key == 40 || e.key == "ArrowDown") {
+//         popChild();
+//     } else {
+//         console.log(e);
+//     }
+// });
+
+
+
+
+
+
+
+/*
+const set =[];
 let count = 0;
 // 새 요소 객체 만들어서 자식으로 추가하기
 pushBtn.addEventListener('click', (e) => {
@@ -15,7 +96,7 @@ pushBtn.addEventListener('click', (e) => {
 
     // 3. Element node를 설정한다
     newDiv.classList.add('material-symbols-outlined');
-    newDiv.style.fontSize = '30px';
+    newDiv.style.fontSize = '35px';
     newDiv.style.color = 'red';
     newDiv.style.display = 'flex';
 
@@ -24,7 +105,10 @@ pushBtn.addEventListener('click', (e) => {
     newDiv.appendChild(newText);
     newDiv.appendChild(addCountDiv());
     // 5. 새 Element node를 out에 추가한다
-    out.appendChild(newDiv);
+    set.push(newDiv);
+    for(let i = 0; i < set.length ; i++){
+        out.appendChild(set[i]);
+    }
 })
 
 // popBtn.addEventListener('click', (e) => {
@@ -42,8 +126,6 @@ pushBtn.addEventListener('click', (e) => {
 //     }
 // })
 
-const shiftBtn = document.querySelector('#shiftBtn');
-const unShiftBtn = document.querySelector('#unShiftBtn');
 
 
 
@@ -73,7 +155,7 @@ function addNewDiv() {
     const newText = document.createTextNode('Grade');
 
     newDiv.classList.add('material-symbols-outlined');
-    newDiv.style.fontSize = '30px';
+    newDiv.style.fontSize = '35px';
     newDiv.style.color = 'red';
     newDiv.style.display = 'flex';
 
@@ -91,15 +173,17 @@ unShiftBtn.addEventListener('click', (e) => {
         const newText = document.createTextNode('Grade');
 
         newDiv.classList.add('material-symbols-outlined');
-        newDiv.style.fontSize = '30px';
+        newDiv.style.fontSize = '35px';
         newDiv.style.color = 'red';
         newDiv.style.display = 'flex';
 
 
         newDiv.appendChild(newText);
         newDiv.appendChild(addCountDiv());
-        
-        out.firstChild.appendChild(newDiv);
+        set.unshift(newDiv);
+        for(let i = 0; i < set.length ; i++){
+            out.appendChild(set[i]);
+        }
     }
 })
 
@@ -107,7 +191,8 @@ unShiftBtn.addEventListener('click', (e) => {
 
 shiftBtn.addEventListener('click', (e) => {
     if (e.target === shiftBtn) {
-        out.removeChild(out.childNodes[0]);
+        set.shift();
+        out.removeChild(out.firstChild);
     }
 })
 
@@ -115,7 +200,9 @@ shiftBtn.addEventListener('click', (e) => {
 
 popBtn.addEventListener('click', (e) => {
     if (e.target === popBtn) {
-        out.removeChild(out.childNodes[out.children.length - 1]);
+        set.pop();
+        out.removeChild(out.lastChild);
     }
 })
 
+*/
