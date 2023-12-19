aa56489d0d8a7e5c85f14b1f1520565a7808f6bb
PigKidney <tony577@naver.com>
2023-12-20 01:42:46 +0900

    23/12/20 am

--- js/resource/js23.js
+++ js/resource/js23.js
@@ -19,6 +19,7 @@ console.log(data3);
 
 const grid = document.getElementById('grid');
 const boxs = document.querySelector('grid>:first-child');
+const secondStage = document.createElement('div');
 
 let boxCount = 1;
 
@@ -33,7 +34,7 @@ function createFristSpan() {
     const span = document.createElement('span');
     span.innerText = boxCount++
     span.classList.add('box');
-    span.style.zIndex = 99;
+    // span.style.zIndex = 99;
     return span;
 }
 
@@ -43,7 +44,7 @@ function createSecondSpan() {
     span.innerText = boxCount++
     span.classList.add('box');
     span.classList.add('second');
-    span.style.zIndex = 60;
+    // span.style.zIndex = 60;
     return span;
 }
 
@@ -62,25 +63,53 @@ for (let i = 0; i < 25; i++) {
 shuffle(setBox);
 for (let i = 0; i < 25; i++) {
     grid.appendChild(setBox[i]);
+
 }
 
-let nextBox=[];
-
-for (let i = 0; i < 25; i++) {
-    nextBox.push(createSecondSpan());
+let nextBox = [];
+for (let i = 26; i <= 50; i++) {
+    nextBox.push(i);
 }
 shuffle(nextBox);
 
-for (let i = 0; i < 25; i++) {
-    grid.appendChild(nextBox[i]);
-}
-
 let goleNum = 1;
+let x = 0;
 grid.addEventListener('click', (e) => {
-    if(e.target.innerText == goleNum){
-        e.target.remove();
-        // e.target.appendChild(nextBox.shift());
+    if (e.target.innerText == goleNum) {
+        if (e.target.innerText === '50') {
+            e.target.innerText = '';
+            clearInterval(interval_id1);
+        }
+        if (nextBox.length === 0) {
+            e.target.innerText = '';
+        } else {
+            e.target.innerText = nextBox[0];
+            nextBox.shift();
+        }
+        // const sec = Math.floor(diff / 1000);
+        const interval_id1 = setInterval(() => document.getElementById('timer').innerHTML = msToTime(`${x++ * 10}`), 10);
         goleNum++;
     }
 });
 
+// setInterval : 정해진 초마다 계속 실행 
+
+// clearTimeout(handler) : 해당 타임아웃 삭제
+
+
+// clearInterval(handler) : 해당 인터벌 삭제
+
+
+
+function msToTime(duration) {
+    var milliseconds = parseInt((duration % 1000) / 100)
+        , seconds = parseInt((duration / 1000) % 60)
+        , minutes = parseInt((duration / (1000 * 60)) % 60)
+        , hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
